2ff566bc75ec9a4070aa5d00bbdf6af405162299
Cameron Saul <cameron@getluckybird.com>
2013-06-17 03:16:30 -0700

    added fetch script

---
+++ fetch-site.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "Make sure you have the Clojure server running on port 3000, b! 'lein ring server'"
+
+# do a recursive fetch of all the site files
+wget -r "localhost:3000" -nH
+
+git commit -am "updated output"
+git push
